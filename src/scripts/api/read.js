53bd5d1f215675ceaf4726ce7aa63823b0d119a0
victor-raphael17 <limaracingteam@gmail.com>
2026-03-17 13:51:35 -0300

    add error treatment at read.js and add flow.html

--- src/scripts/api/read.js
+++ src/scripts/api/read.js
@@ -2,5 +2,9 @@ export async function getUsers(apiUrl) {
     const response = await fetch(apiUrl);
     const data = await response.json();
 
+    if (!response.ok) {
+        throw new Error(data.error || 'Failed to fetch users');
+    }
+
     return data.users;
 }
